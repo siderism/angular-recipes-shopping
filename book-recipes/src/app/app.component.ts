bedd8a4aed926d4c7d5e708e7b69ff587c9a5e1b
Michalis Sideris <m.sideris@epsilon-singularlogic.eu>
2022-07-08 15:16:33 +0300

    added data bindings

--- book-recipes/src/app/app.component.ts
+++ book-recipes/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-recipes';
+  loadedFeature = 'recipe'
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
--- book-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ book-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Some details about ths recipe', 'https://shop.foodbox.co.nz/files/img/recipe/basiccurry.jpg')
   ];
@@ -15,4 +16,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
