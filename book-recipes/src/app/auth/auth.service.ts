e2ef448c4c02c97090f0b6799eb490a2b707e782
Michalis Sideris <m.sideris@epsilon-singularlogic.eu>
2022-07-22 14:43:37 +0300

    added api key to environment

--- book-recipes/src/app/auth/auth.service.ts
+++ book-recipes/src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { tap } from "rxjs/operators";
 import { BehaviorSubject, catchError, throwError } from "rxjs";
 import { User } from "./user.model";
 import { Router } from "@angular/router";
+import { environment } from "src/environments/environment";
 
 export interface AuthResponseData {
     kind: string,
@@ -25,7 +26,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router) {}
 
     signUp(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC1z80EmsB8tpFMf2mD99-NmXCTYswwxew', {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password,
             returnSecureToken: true
@@ -35,7 +36,7 @@ export class AuthService {
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC1z80EmsB8tpFMf2mD99-NmXCTYswwxew', {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password,
             returnSecureToken: true
--- book-recipes/src/environments/environment.ts
+++ book-recipes/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyC1z80EmsB8tpFMf2mD99-NmXCTYswwxew'
 };
 
 /*
